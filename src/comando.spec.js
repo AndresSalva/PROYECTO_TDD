ff1a5b4b2be146e439bf90d232dca2b18360c83a
AndresSalva <andres.salvatierra@ucb.edu.bo>
2024-09-01 16:03:32 -0400

    Aniadiendo clase superficie y mostrando area

--- src/comando.spec.js
+++ src/comando.spec.js
@@ -1,4 +1,5 @@
 import mostrarComando from "./comando.js";
+import Superficie from "./superficie.js";
 
 describe("Linea de comandos del auto", () => {
   it("Deberia mostrar el comando ingresado por el usuario", () => {
@@ -6,3 +7,9 @@ describe("Linea de comandos del auto", () => {
     expect(result).toEqual("5,5/1,2N/IAIAIAIAA");
   });
 });
+describe("Superficie", () => {
+    it("Deberia mostrar el area de la superfice", () => {
+      const result = new Superficie(3, 3)
+      expect(result.mostrarSuperficie()).toEqual([3, 3]);
+    });
+  });
