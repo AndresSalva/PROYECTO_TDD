63db70c62eb0be08a708c6ec5dad0962f15b5759
Camila Rosas <camila.rosas@ucb.edu.bo>
2024-09-02 20:39:19 -0400

    Modificar IU para mostrar la posicion final

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,4 @@
-import mostrarComando from "./comando";
+import {mostrarComando,ejecutar} from "./comando";
 
 const comando = document.querySelector("#comando");
 const form = document.querySelector("#comando-form");
@@ -8,7 +8,7 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const resultado = comando.value
-  //const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p> Comando realizado: " + mostrarComando(resultado) + "</p>";
+  div.innerHTML = "<p> Comando realizado: " + mostrarComando(resultado) + "</br> Posicion final: " + ejecutar(resultado) +"</p>";
+
 });
